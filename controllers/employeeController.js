a0d809010e75c7d56a407b6b989c1b607991c170
hiren1999 <hiren.thinktanker@gmail.com>
2021-04-27 18:13:14 +0530

    crud-done

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -12,7 +12,6 @@ const fs = require('fs');
 // @access    Private
 exports.createEmployee = async (req, res, next) => {
     try {
-        // console.log(req.body, 'body');
         const { errors, isValid } = await validateRegisterInput(req.body);
 
         // Check here Username isExists or not
@@ -54,7 +53,6 @@ exports.createEmployee = async (req, res, next) => {
 
             const filetypes = /jpg|JPG|jpeg|JPEG|png|PNG|GIF|gif/;
             const check_image = !filetypes.test(extension);
-            console.log('exrtension1::', extension);
 
             if (check_image) {
                 errors.profilephoto = 'Only image files are allowed';
@@ -68,7 +66,6 @@ exports.createEmployee = async (req, res, next) => {
             response.errors = errors;
             return res.status(422).json(response);
         } else {
-            console.log('exrtension2::::::::::::::', extension);
             var filepath = '/upload/Profile_Photo/';
             var publicpath = process.cwd() + '/public';
             var storepath = publicpath + filepath;
@@ -153,29 +150,154 @@ exports.getEmployee = async (req, res, next) => {
 // @desc      Update Employee
 // @route     PATCH /employees/edit/:id
 // @access    Private
-exports.updateEmployee = asyncHandler(async (req, res, next) => {
-    let employee = await Employee.findById(req.params.id);
+exports.updateEmployee = async (req, res, next) => {
+    // let employee = await Employee.findById(req.params.id);
 
-    if (!employee) {
-        return next(
-            new ErrorResponse(
-                `Employee not found with id of ${req.params.id}`,
-                404
-            )
+    //     if (!employee) {
+    //         return next(
+    //             new ErrorResponse(
+    //                 `Employee not found with id of ${req.params.id}`,
+    //                 404
+    //             )
+    //         );
+    //     }
+
+    //     employee = await Employee.findByIdAndUpdate(req.params.id, req.body, {
+    //         new: true,
+    //         runValidators: true,
+    //     });
+
+    try {
+        var url = req.originalUrl.split('/');
+        const { errors, isValid } = await validateRegisterInput(
+            req.body,
+            url[2]
         );
+
+        Employee.findOne({ username: req.body.username }).then(
+            (check_username) => {
+                console.log('check', req.params.id);
+                if (
+                    check_username &&
+                    isEmpty(errors.username) &&
+                    check_username._id != req.params.id
+                ) {
+                    errors.username = 'User already exist';
+                }
+                Employee.findOne({ email: req.body.email }).then(
+                    (check_email) => {
+                        if (
+                            check_email &&
+                            isEmpty(errors.email) &&
+                            check_email._id != req.params.id
+                        ) {
+                            errors.email = 'Email already exist';
+                        }
+
+                        if (!isEmpty(req.body.profilephoto)) {
+                            var matches = req.body.profilephoto.match(
+                                    /^data:([A-Za-z-+\/]+);base64,(.+)$/
+                                ),
+                                image = {};
+
+                            image.type = matches[1];
+                            image.data = new Buffer.from(matches[2], 'base64');
+                            let decodedImg = image;
+                            var imageBuffer = decodedImg.data;
+                            let type = decodedImg.type;
+                            var extension = mime.getExtension(type);
+
+                            var filetypes = /jpg|JPG|jpeg|JPEG|png|PNG|GIF|gif/;
+                            var check_image = !filetypes.test(extension);
+
+                            if (check_image) {
+                                errors.profilephoto =
+                                    'Only image files are allowed';
+                            }
+                        }
+
+                        if (!isValid || !isEmpty(errors)) {
+                            var response = {};
+                            response.error = true;
+                            response.message = 'missing some field';
+                            response.errors = errors;
+                            return res.status(422).json(response);
+                        } else {
+                            Employee.findOne({ _id: req.params.id }).exec(
+                                function (err, employee) {
+                                    if (!isEmpty(req.body.profilephoto)) {
+                                        var filepath = '/upload/Profile_Photo/';
+
+                                        var publicpath =
+                                            process.cwd() + '/public';
+                                        var storepath = publicpath + filepath;
+
+                                        fse.mkdirsSync(storepath);
+
+                                        var filename =
+                                            Date.now() +
+                                            '-DP' +
+                                            '.' +
+                                            extension;
+                                        fs.writeFileSync(
+                                            storepath + filename,
+                                            imageBuffer,
+                                            'utf8'
+                                        );
+
+                                        old_file =
+                                            storepath + employee.profilephoto;
+
+                                        fs.unlink(old_file, (err) => {
+                                            if (err) {
+                                                console.error(err);
+                                            }
+                                        });
+                                    }
+
+                                    let employee_data = {};
+                                    employee_data.username = req.body.username;
+                                    employee_data.email = req.body.email;
+                                    employee_data.firstname =
+                                        req.body.firstname;
+                                    employee_data.lastname = req.body.lastname;
+                                    employee_data.mobileno = req.body.mobileno;
+                                    employee_data.dob = req.body.dob;
+                                    employee_data.hobbies = req.body.hobbies;
+                                    employee_data.gender = req.body.gender;
+
+                                    if (!isEmpty(req.body.profilephoto)) {
+                                        employee_data.profilephoto = filename;
+                                    }
+
+                                    let query = { _id: req.params.id };
+                                    Employee.updateOne(
+                                        query,
+                                        employee_data,
+                                        function (err) {
+                                            if (err) {
+                                                console.log(err);
+                                            } else {
+                                                var response = {};
+                                                response.success = true;
+                                                response.error = false;
+                                                response.message =
+                                                    'Employee Record updated Successfully';
+                                                res.status(200).json(response);
+                                            }
+                                        }
+                                    );
+                                }
+                            );
+                        }
+                    }
+                );
+            }
+        );
+    } catch (error) {
+        console.log(error, 'ERROR');
     }
-
-    employee = await Employee.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true,
-    });
-
-    // res.status(200).json({ success: true, data: employee });
-    res.render('employee/edit', {
-        req: req,
-        response: employee,
-    });
-});
+};
 
 // @desc      Delete employee
 // @route     DELETE /employees/:id
